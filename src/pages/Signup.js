138c38bfd5386d6c3931f101f61f8855ca91ee58
jjolraman <jjolraman@gmail.com>
2023-02-25 17:39:33 +0900

    signup page working

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,26 +1,56 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useMutation } from 'react-query';
+import { useCookies } from 'react-cookie';
 import axios from 'axios';
 
 // 리액트쿠키 라이브러리?
 
 function Signup() {
-  const [id, setId] = useState();
-  const [pw, setPw] = useState();
-  const [nickname, setNickname] = useState();
+  const [state, setState] = useState({ id: '', pw: '', nickname: '' });
+  const [cookies, setCookie] = useCookies(['userToken']);
+
+  const handleSignup = async () => {
+    try {
+      const { data } = await axios.post('http://localhost:3000/signup', state);
+      setCookie('userToken', data.token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   return (
     <StDiv>
       <h1>회원가입</h1>
       <div>
-        <StInput type="text" placeholder="아이디" />
+        <StInput
+          type="text"
+          name="id"
+          placeholder="아이디"
+          value={state.id}
+          onChange={handleChange}
+        />
         <br></br>
-        <StInput type="text" placeholder="비밀번호" />
+        <StInput
+          type="password"
+          name="pw"
+          placeholder="비밀번호"
+          value={state.pw}
+          onChange={handleChange}
+        />
         <br></br>
-        <StInput type="text" placeholder="닉네임" />
+        <StInput
+          type="text"
+          name="nickname"
+          placeholder="닉네임"
+          value={state.nickname}
+          onChange={handleChange}
+        />
         <br></br>
-        <StBtn>회원가입</StBtn>
+        <StBtn onClick={handleSignup}>회원가입</StBtn>
       </div>
     </StDiv>
   );
@@ -33,7 +63,7 @@ const StDiv = styled.div`
   padding: 5px;
   text-align: center;
   background-color: antiquewhite;
-  border: 3px solid red;
+  border: 3px solid transparent;
   margin: 50px;
 `;
 
