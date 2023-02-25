22cf6fd30d5e830047a23df71f86fd3940074261
jjolraman <jjolraman@gmail.com>
2023-02-25 09:46:12 +0900

    test test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <StDiv>
+      <h1>로그인</h1>
+      <div>
+        <StInput type="text" placeholder="아이디" />
+        <br></br>
+        <StInput type="text" placeholder="비밀번호" />
+        <br></br>
+        <StBtn>로그인</StBtn>
+      </div>
+    </StDiv>
+  );
 }
 
-export default Login
+export default Login;
+
+const StDiv = styled.div`
+  height: 50px;
+  padding: 5px;
+  text-align: center;
+  background-color: antiquewhite;
+  border: 3px solid red;
+  margin: 50px;
+`;
+
+const StInput = styled.input`
+  height: 50px;
+  margin: 10px;
+`;
+
+const StBtn = styled.button`
+  background-color: gray;
+`;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useMutation } from 'react-query';
+import axios from 'axios';
+
+// 리액트쿠키 라이브러리?
 
 function Signup() {
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+  const [nickname, setNickname] = useState();
+
   return (
-    <div>Signup</div>
-  )
+    <StDiv>
+      <h1>회원가입</h1>
+      <div>
+        <StInput type="text" placeholder="아이디" />
+        <br></br>
+        <StInput type="text" placeholder="비밀번호" />
+        <br></br>
+        <StInput type="text" placeholder="닉네임" />
+        <br></br>
+        <StBtn>회원가입</StBtn>
+      </div>
+    </StDiv>
+  );
 }
 
-export default Signup
+export default Signup;
+
+const StDiv = styled.div`
+  height: 50px;
+  padding: 5px;
+  text-align: center;
+  background-color: antiquewhite;
+  border: 3px solid red;
+  margin: 50px;
+`;
+
+const StInput = styled.input`
+  height: 50px;
+  margin: 10px;
+`;
+
+const StBtn = styled.button`
+  background-color: gray;
+`;
