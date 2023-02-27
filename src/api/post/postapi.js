17320cc12df1d936a79b58b8a63f17cc9ab46b61
jaeuk Lee <lskkhj1005@naver.com>
2023-02-27 23:22:54 +0900

    feat: connect with server / temporary

--- src/api/post/postapi.js
+++ src/api/post/postapi.js
@@ -1,14 +1,11 @@
 // 게시물 조회, 수정, 삭제, 등록 api 모음
 
-// https://bc91bf5e-9081-4e01-a1c1-00f5792f2bca.mock.pstmn.io
-// http://13.209.86.39:3003
-
 import axios from 'axios';
 
 // 게시물 조회        /api/posts
 const getPosts = async () => {
   const response = await axios.get(
-    'https://478e2b2f-81a1-41f5-9f8d-3748e8184bd0.mock.pstmn.io/api/posts'
+    `${process.env.REACT_APP_SERVER}/api/posts`
   );
   console.log("response", response)
   return response.data.data
@@ -18,7 +15,7 @@ const getPosts = async () => {
 const getDetailPost = async (id) => {
   const postId = Number(id);
   const detailresponse = await axios.get(
-    `https://478e2b2f-81a1-41f5-9f8d-3748e8184bd0.mock.pstmn.io/api/posts/1`
+    `${process.env.REACT_APP_SERVER}/api/posts/1`
   );
   // console.log(detailresponse)
   return detailresponse.data.data;
@@ -27,7 +24,7 @@ const getDetailPost = async (id) => {
 // 게시물 추가    /api/posts (O)
 const addPost = async (newPost) => {
   await axios.post(
-    'https://478e2b2f-81a1-41f5-9f8d-3748e8184bd0.mock.pstmn.io/api/posts',
+    `${process.env.REACT_APP_SERVER}/api/posts`,
     newPost,
     {
       'Content-Type': 'multipart/form-data',
@@ -39,7 +36,7 @@ const addPost = async (newPost) => {
 const deletePost = async (id) => {
   const postId = Number(id);
   await axios.delete(
-    `https://478e2b2f-81a1-41f5-9f8d-3748e8184bd0.mock.pstmn.io/api/posts/${postId}`
+    `${process.env.REACT_APP_SERVER}/api/posts/${postId}`
   );
 };
 
@@ -47,7 +44,7 @@ const deletePost = async (id) => {
 const updatePost = async ({ id, inputTitle, inputContent }) => {
   // 받아올 때도 중괄호
   await axios.put(
-    `https://478e2b2f-81a1-41f5-9f8d-3748e8184bd0.mock.pstmn.io/api/posts/${id}`,
+    `${process.env.REACT_APP_SERVER}/api/posts/${id}`,
     {
       title: `${inputTitle}`,
       content: `${inputContent}`,
@@ -58,7 +55,7 @@ const updatePost = async ({ id, inputTitle, inputContent }) => {
 // 좋아요 기능    /api/likes/posts/:postId (O)
 const likeUp = async (id) => {
   const postId = Number(id);
-  await axios.put(`https://478e2b2f-81a1-41f5-9f8d-3748e8184bd0.mock.pstmn.io/api/likes/posts/${postId}`);
+  await axios.put(`${process.env.REACT_APP_SERVER}/api/likes/posts/${postId}`);
 };
 
 export { getPosts, addPost, deletePost, getDetailPost, updatePost, likeUp };
